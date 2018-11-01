a9eca462818c61638cbe93812b816dfa046cdf82
coolprogrammer29 <pjdavies29@outlook.co.nz>
2018-11-01 21:43:00 +1300

    v

--- MyFirstProject/src/HelloWorldExample.java
+++ MyFirstProject/src/HelloWorldExample.java
@@ -2,7 +2,7 @@
 public class HelloWorldExample {
 
 	public static void main(String[] args) {
-		// Output Hello World to the user.
+		//Output Hello World to the USer
 		System.out.println("Hello World");
 
 	}
